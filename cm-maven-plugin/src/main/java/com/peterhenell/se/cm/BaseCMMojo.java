659926f2302c6a0ce366f29cd1e1abf844e16c4e
Henell, Peter <peter.henell@sek.se>
2017-05-09 16:46:07 +0200

    trying out jgit

--- cm-maven-plugin/src/main/java/com/peterhenell/se/cm/BaseCMMojo.java
+++ cm-maven-plugin/src/main/java/com/peterhenell/se/cm/BaseCMMojo.java
@@ -1,8 +1,6 @@
-package sample.plugin;
+package com.peterhenell.se.cm;
  
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
  
 
--- cm-maven-plugin/src/main/java/com/peterhenell/se/cm/CreatePackageMojo.java
+++ cm-maven-plugin/src/main/java/com/peterhenell/se/cm/CreatePackageMojo.java
@@ -1,63 +1,75 @@
-package sample.plugin;
- 
-import org.apache.maven.plugin.AbstractMojo;
+package com.peterhenell.se.cm;
+
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
- 
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
 /**
  * Says "Hi" to the user.
  *
  */
-@Mojo( name = "createPackage")
-public class CreatePackageMojo extends BaseCMMojo
-{
+@Mojo(name = "createPackage")
+public class CreatePackageMojo extends BaseCMMojo {
     /**
      * The greeting to display.
      */
-    @Parameter( property = "createPackage.version", defaultValue = "${project.version}" )
+    @Parameter(property = "createPackage.version", defaultValue = "${project.version}")
     private String version;
-    
-    /**
-     * The greeting to display.
-     */
-    @Parameter( property = "createPackage.project", defaultValue = "${project.groupId}.${project.artifactId}" )
-    private String project;
-    
-    /**
-     * The greeting to display.
-     */
-    @Parameter( property = "createPackage.buildDate", defaultValue = "Hello World!" )
-    private String buildDate;
-    
-    /**
-     * The greeting to display.
-     */
-    @Parameter( property = "createPackage.localBuildPath", defaultValue = "${project.basedir}" )
-    private String localBuildPath;
-    
-    /**
-     * The greeting to display.
-     */
-    @Parameter( property = "createPackage.pathToGitRepo", defaultValue = "${project.basedir}" )
-    private String pathToGitRepo;
-    
-    /**
-     * The greeting to display.
-     */
-    @Parameter( property = "createPackage.branch", defaultValue = "master" )
-    private String branch;
-    
-    /**
-     * The greeting to display.
-     */
-    @Parameter( property = "createPackage.packageStatus", defaultValue = "Building" )
-    private String packageStatus;
-    
-    
 
-    public void execute() throws MojoExecutionException
-    {
-        getLog().info( project );
+    /**
+     * The greeting to display.
+     */
+    @Parameter(property = "createPackage.project", defaultValue = "${project.groupId}.${project.artifactId}")
+    private String project;
+
+    /**
+     * The greeting to display.
+     */
+    @Parameter(property = "createPackage.buildDate", defaultValue = "Hello World!")
+    private String buildDate;
+
+    /**
+     * The greeting to display.
+     */
+    @Parameter(property = "createPackage.localBuildPath", defaultValue = "${project.basedir}")
+    private String localBuildPath;
+
+    /**
+     * The greeting to display.
+     */
+    @Parameter(property = "createPackage.pathToGitRepo", defaultValue = "${project.basedir}")
+    private String pathToGitRepo;
+
+    /**
+     * The greeting to display.
+     */
+    @Parameter(property = "createPackage.branch", defaultValue = "master")
+    private String branch;
+
+    /**
+     * The greeting to display.
+     */
+    @Parameter(property = "createPackage.packageStatus", defaultValue = "Building")
+    private String packageStatus;
+
+    public void execute() throws MojoExecutionException {
+        getLog().info(project);
+
+        try {
+            Repository existingRepo = new FileRepositoryBuilder().setGitDir(new File(pathToGitRepo+"\\.git")).build();
+            Ref master = existingRepo.getRef("master");
+            ObjectId id = master.getObjectId();
+            getLog().info(id.toString());
+
+        } catch (IOException ex) {
+            getLog().error("could not get git repo" + ex.getMessage());
+        }
     }
 }
